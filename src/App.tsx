f3464ca6f72859f33f228a9db87653c53790c5f5
Thábata Lima <64754404+St4rThabs@users.noreply.github.com>
2022-10-16 12:43:08 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './styles/global.css';
+
+export function App() {
+
+  return (
+    <>
+      <h1>Hello World</h1>
+      <button className="bg-cyan-500 font-medium px-4 py-2 rounded hover:bg-cyan-300">Enviar</button>
+    </>
+  )
+}
+
+export const Default = {}
